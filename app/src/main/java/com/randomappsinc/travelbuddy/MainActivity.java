86f0657dc5faa290d6c8458aa852c7a1ef8c6733
Gear61 <chessnone@yahoo.com>
2019-10-27 21:04:41 +0200

    Cleanup

--- app/src/main/java/com/randomappsinc/travelbuddy/MainActivity.java
+++ app/src/main/java/com/randomappsinc/travelbuddy/MainActivity.java
@@ -1,7 +1,8 @@
 package com.randomappsinc.travelbuddy;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
